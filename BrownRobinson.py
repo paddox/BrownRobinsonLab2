a3576ded729b721bee29cb156978f248536ee237
paddox <paddox@yandex.ru>
2018-10-10 18:56:48 +0300

    Метод Брауна-Робинсона

---
+++ BrownRobinson.py
@@ -0,0 +1,80 @@
+strategyMatrix = [[2, 1, 3],
+                    [3, 0, 1],
+                    [1, 2, 1]]
+
+def indexOfMaxElement(array):
+    return sorted([(index, value) for (index, value) in enumerate(array)], key=lambda x: x[1])[-1][0]
+
+def indexOfMinElement(array):
+    return sorted([(index, value) for (index, value) in enumerate(array)], key=lambda x: x[1])[0][0]
+
+def BrownRobinson(strategyMatrix, targetError, maxStep):
+    currentStrategyA = 0
+    currentStrategyB = 0
+    algSteps = []
+    total = {"solution": [], "answer": []}
+    M = len(strategyMatrix)
+    N = len(strategyMatrix[0])
+    CHOICE_A_INDEX = 0
+    CHOICE_B_INDEX = 1
+    WIN_A_START = 2
+    WIN_B_START = WIN_A_START + M
+    TOP_SCORE = WIN_B_START + N
+    LOWER_SCORE = TOP_SCORE + 1
+    ERROR = LOWER_SCORE + 1
+    algSteps.append([0 for i in range(ERROR+1)])
+    minTopScore = 0
+    maxLowerScore = 0
+    for i in range(1, maxStep):
+        currentStep = [0 for k in range(ERROR+1)]
+        currentStep[CHOICE_A_INDEX] = currentStrategyA + 1
+        currentStep[CHOICE_B_INDEX] = currentStrategyB + 1
+        for j in range(M):
+            currentStep[WIN_A_START + j] = algSteps[i-1][WIN_A_START + j] + strategyMatrix[j][currentStrategyB]
+        for j in range(N):
+            currentStep[WIN_B_START + j] = algSteps[i-1][WIN_B_START + j] + strategyMatrix[currentStrategyA][j]
+        currentStep[TOP_SCORE] = max(currentStep[WIN_A_START:WIN_A_START+M]) / i
+        currentStep[LOWER_SCORE] = min(currentStep[WIN_B_START:WIN_B_START+N]) / i
+        if i==1:
+            minTopScore = currentStep[TOP_SCORE]
+            maxLowerScore = currentStep[LOWER_SCORE]
+        else:
+            if currentStep[TOP_SCORE] < minTopScore:
+                minTopScore = currentStep[TOP_SCORE]
+            if currentStep[LOWER_SCORE] > maxLowerScore:
+                maxLowerScore = currentStep[LOWER_SCORE]
+        currentStep[ERROR] = minTopScore - maxLowerScore
+        algSteps.append(currentStep)
+        print(' '.join([str(item) for item in currentStep]))
+        if currentStep[ERROR] < targetError:
+            print(0.5*(minTopScore + maxLowerScore))
+            x = [a[0] for a in algSteps]
+            y = [a[1] for a in algSteps]
+            answer = [[],[]]
+            for k in range(1, M+1):
+                print('{0}: {1}'.format(k, x.count(k)/(len(x)-1)))
+                answer[0].append(x.count(k)/(len(x)-1))
+            for k in range(1, N+1):
+                print('{0}: {1}'.format(k, y.count(k)/(len(y)-1)))
+                answer[1].append(y.count(k)/(len(y)-1))
+            answer.append(currentStep[ERROR])
+            total["solution"] = algSteps
+            total["answer"] = answer
+            return total
+        currentStrategyA = indexOfMaxElement(currentStep[WIN_A_START:WIN_A_START+M])
+        currentStrategyB = indexOfMinElement(currentStep[WIN_B_START:WIN_B_START+N])
+    total["solution"].append(algSteps)
+    return total
+
+print(BrownRobinson(strategyMatrix, 0.01, 100))
+
+a = [2, 3, 1]
+b = sorted([(index, value) for (index, value) in enumerate(a)], key=lambda x: x[1])[-1][0]
+print(b)
+
+#python -m PyQt5.uic.pyuic -x D:\Projects\Lab2\mainWindow.ui -o D:\Projects\Lab2\mainWindow.py
+    
+        
+
+
+    
---
+++ main.py
@@ -0,0 +1,73 @@
+import sys  # sys нужен для передачи argv в QApplication
+from PyQt5 import QtWidgets, QtCore
+import mainWindow
+import BrownRobinson
+
+class Application(QtWidgets.QMainWindow, mainWindow.Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)  # Это нужно для инициализации нашего дизайна
+        self.pushButtonCalculate.clicked.connect(self.calculate)
+        self.spinBoxStrategyACount.valueChanged.connect(self.spinBoxAValueChanged)
+        self.spinBoxStrategyBCount.valueChanged.connect(self.spinBoxBValueChanged)
+
+    def calculate(self):
+        strategyMatrix = []
+        #Считываем входные данные
+        for i in range(self.tableWidgetStrategyMatrixInput.rowCount()):
+            strategyMatrix.append([])
+            for j in range(self.tableWidgetStrategyMatrixInput.columnCount()):
+                if self.tableWidgetStrategyMatrixInput.item(i, j) != None:
+                    strategyMatrix[i].append(int(self.tableWidgetStrategyMatrixInput.item(i, j).text()))
+                else:
+                    strategyMatrix[i].append(0)
+        maxError = float(self.lineEditMaxError.text())
+        maxIteration = int(self.spinBoxMaxIterationCount.value())
+        total = BrownRobinson.BrownRobinson(strategyMatrix, maxError, maxIteration)
+        solution = total["solution"]
+        answer = total["answer"]
+        print(solution)
+        print(maxError)
+        print(maxIteration)
+        print(strategyMatrix)
+        #Вывыд пошагового решения
+        self.tableWidgetSolution.setRowCount(len(solution) - 1)
+        self.tableWidgetSolution.setColumnCount(len(solution[0]))
+        header = ["Выбор\nА", "Выбор\nБ"]
+        header.extend(["x{}".format(a+1) for a in range(self.spinBoxStrategyACount.value())])
+        header.extend(["y{}".format(a+1) for a in range(self.spinBoxStrategyBCount.value())])
+        header.extend(["Верхняя\nоценка", "Нижняя\nоценка", "Ошибка"])
+        self.tableWidgetSolution.setHorizontalHeaderLabels(header)
+        for i in range(1, len(solution)):
+            for j in range(len(solution[0])):
+                addItem = solution[i][j] if type(solution[i][j])=='int' else round(solution[i][j], 4)
+                self.tableWidgetSolution.setItem(i-1, j, QtWidgets.QTableWidgetItem(str(addItem)))
+        self.tableWidgetSolution.resizeColumnsToContents()
+
+        self.tableWidgetAnswer.setRowCount(1)
+        self.tableWidgetAnswer.setColumnCount(len(answer[0])+len(answer[1])+1)
+        headerAnswer = ["x{}".format(a+1) for a in range(len(answer[0]))]
+        headerAnswer.extend(["y{}".format(a+1) for a in range(len(answer[1]))])
+        headerAnswer.append("Ошибка")
+        self.tableWidgetAnswer.setHorizontalHeaderLabels(headerAnswer)
+        for i in range(len(answer[0])):
+            self.tableWidgetAnswer.setItem(0, i, QtWidgets.QTableWidgetItem(str(round(answer[0][i], 3))))
+        
+        for i in range(len(answer[0]), len(answer[0])+len(answer[1])):
+            self.tableWidgetAnswer.setItem(0, i, QtWidgets.QTableWidgetItem(str(round(answer[1][i-len(answer[0])], 3))))
+        
+        self.tableWidgetAnswer.setItem(0, len(answer[0])+len(answer[1]), QtWidgets.QTableWidgetItem(str(round(answer[2], 4))))
+        self.tableWidgetAnswer.resizeColumnsToContents()
+
+
+    def spinBoxAValueChanged(self):
+        self.tableWidgetStrategyMatrixInput.setRowCount(self.spinBoxStrategyACount.value()) 
+
+    def spinBoxBValueChanged(self):
+        self.tableWidgetStrategyMatrixInput.setColumnCount(self.spinBoxStrategyBCount.value()) 
+
+if __name__ == '__main__':  # Если мы запускаем файл напрямую, а не импортируем
+    app = QtWidgets.QApplication(sys.argv)  # Новый экземпляр QApplication
+    window = Application()  # Создаём объект класса ExampleApp
+    window.show()  # Показываем окно
+    app.exec_()
